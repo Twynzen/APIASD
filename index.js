88b9b12ed47262bc50e80ff81abb586f2eec1d3c
Daniel Armando Castiblanco Fajardo <44913158+Twynzen@users.noreply.github.com>
2022-11-12 01:52:14 -0500

    feat: advance endpoints basics

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.hson());
+app.use(express.json());
 
 const mutants = [
     { id: 1, name: 'Spiderman', age: '20', Vehicle: 'Urban bike', isAlive: true, insidePrision: false },
@@ -12,13 +12,38 @@ const mutants = [
 app.get('/', (req, res) => {
     res.send('API DE MUTANTES')
 });
+app.get('/api/mutants', (req, res) => {
+    res.send(mutants)
+});
 
-app.get('/api/mutants:id', (req, res) => {
-    const mutant = mutants.find(everyMutant => everyMutant.id === parseInt(req, params.id));
-    res.send('API DE MUTANTES');
+app.get('/api/mutants/:id', (req, res) => {
+    const mutant = mutants.find(everyMutant => everyMutant.id === parseInt(req.params.id));
     if (!mutant) {
-        return res.status(404).send('Estudiante no encontrado');
+        return res.status(404).send('Mutante no encontrado');
     } else {
         res.send(mutant);
     }
 });
+
+app.post('api/mutants', (req, res) => {
+    const mutant = {
+        id: mutants.length + 1,
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        vehicle: (req.body.vehicle),
+        isAlive: req.body.isAlive,
+        insidePrision: req.body.insidePrision
+    }
+    mutants.push(mutant);
+    res.send(mutant);
+});
+
+app.delete('api/mutants/:id', (req, res) => {
+    const mutant = mutants.find(everyMutant => everyMutant.id === parseInt(req.params.id));
+    if (!mutant) {
+        return res.status(404).send('Mutante no encontrado')
+    }
+});
+
+const port = process.env.port || 8080;
+app.listen(port, () => console.log(`Listen port ${port}...`));
